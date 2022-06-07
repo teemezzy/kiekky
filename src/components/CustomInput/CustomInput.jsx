f5c9a7c72567263afb5353dd1994c5bdc41c1e7e
Toromo A <toromo.ade06@gmail.com>
2022-06-07 13:54:33 -0500

    commit

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 
 const CustomInput = () => {
-  return (
-    <div>
-      CustomInput
-      </div>
-      );
+  return <div>CustomInput</div>;
 };
 
 export default CustomInput;
 
-
-
-
 export const FooterInput = () => {
   return (
     <div>
-        <input type="text"  />
-        FooterInput
-        </div>
-  )
-}
+      <input type="text" placeholder="Email Address" />
+      <button>Submit</button>
+    </div>
+  );
+};
