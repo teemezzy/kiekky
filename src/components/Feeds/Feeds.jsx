2d63267a5024b170da9b03b08559350a6bd4de23
Toromo A <toromo.ade06@gmail.com>
2022-06-29 16:59:45 -0500

    feeds

--- src/components/Feeds/Feeds.jsx
+++ src/components/Feeds/Feeds.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {MdOutlineLocationOn} from  "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
 import axios from "axios";
 
 function Feeds() {
@@ -20,20 +20,22 @@ function Feeds() {
             >
               <div>
                 <div className="postItem  items-center mb-7 ">
-                  <div className="username flex items-center mx-5 ">
+                  <div className=" flex gap-10 items-center ml-5 ">
                     <div className="img-container">
                       <img
-                        className="mb-2 w-[4rem] h-[4rem]  p-[1.5px] border-[#6a52fd] border-2 rounded-[50%]"
+                        className="mb-2 w-full h-[4rem] p-[1.5px] border-[#6a52fd] border-2 rounded-[50%]"
                         src={feed.image}
                         alt="icon"
                       />
                     </div>
 
-                    <div className="text mx-2">
+                    <div className="lg:w-[42rem] w-[23rem]">
                       <p className="font-[700]">{feed.firstname}</p>
-                      <div className="location flex space-x-24 md:space-x-[17rem] text-gray text-sm w-full">
+                      <div className="location flex justify-between pr-5 text-gray text-sm ">
                         <p className="text-sm flex">
-                          <span> <MdOutlineLocationOn color="gray"/> </span> 
+                          <span>
+                            <MdOutlineLocationOn color="gray" />
+                          </span>
                           {feed.address.country}, {feed.address.city}
                         </p>
                         <p className="text-[gray] hidden lg:block text-sm">
