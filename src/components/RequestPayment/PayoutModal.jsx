345c794f9b093904db48bb22fd0c48aa830c5541
Toromo A <toromo.ade06@gmail.com>
2022-07-13 14:21:08 -0500

    sum

---
+++ src/components/RequestPayment/PayoutModal.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+const PayoutModal = () => {
+
+    // const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div className="w-[699px] h-[668px] m-auto fixed inset-0 flex justify-center items-center">
+        <div className="fixed inset-0 flex justify-center items-center">
+            Payout MODAL
+        </div>
+
+
+        
+
+    </div>
+  )
+}
+
+export default PayoutModal
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Sidebar, UserNav, User, UserInfo, PPV } from "../../components";
 
-
 function UserProfile(props) {
   return (
-    <div className="notifications bg-[#F9FAFA] ">
+    <div className="notifications bg-[#F9FAFA]  ">
       <UserNav />
-      <div className="flex mt-[6rem] bg-[#F9FAFA] ">
+      <div className="flex mt-[6rem] bg-[#F9FAFA] m-auto">
         <div className="  fixed h-[100%] hidden lg:block sidebar_prt pt-8">
           <Sidebar />
         </div>
-        <div className="lg:ml-[20rem] lg:w-[60rem] m-auto ">
-          <User />
-
-          <UserInfo />
-
-          <PPV />
+        <div className="lg:ml-[20rem] lg:w-[60rem] ">
+          <div>
+            <User />
+            <UserInfo />
+            <PPV />
+          </div>
         </div>
       </div>
     </div>
