f70c0c97cd3fdecefbeca103adbd7d6cba72cada
Toromo A <toromo.ade06@gmail.com>
2022-07-19 06:31:31 -0500

    text

--- src/components/PPV/PPV.jsx
+++ src/components/PPV/PPV.jsx
@@ -3,32 +3,32 @@ import ProfilePicture from "./ProfilePicture";
 import ProfilePost from "./ProfilePost";
 import ProfileVideo from "./ProfileVideo";
 
-function UserInfo(props) {
+function UserInfo() {
   const [toggleState, setToggleState] = useState(1);
 
   const toggleTab = (tab) => {
     setToggleState(tab);
-    console.log(tab);
+    // console.log(tab);
   };
   const activeContent = " ";
-  const content = "bg-white w-full hidden p-[20px]";
+  const content = "bg-white w-full hidden p-[20px] ";
 
   return (
     <div>
-      <div className=" flex mt-5  mb-5 sm:items-center lg:items-start sm:w-[100vw] lg:w-[60rem] ">
+      <div className=" flex mt-5  mb-5 items-center lg:items-start w-[100vw] lg:w-[60rem] ">
         <div
           className={`${
-            toggleState === 1 ? "input-option-tab em " : "input-option-tab"
+            toggleState === 1 ? "input-option-tab text-white" : "input-option-tab "
           } flex h-[3rem] `}
         >
           <div
             onClick={() => toggleTab(1)}
             className={`${
               toggleState === 1 ? "login-tab-active2" : ""
-            }  py-[0.3rem] px-[2rem]  self-center  `}
+            }  py-[0.3rem] px-[2rem] self-center`}
           >
             <div>
-              <p className=" text-[#898989]">Post</p>
+              <p className=" text-[#A8A8A8]">Post</p>
             </div>
           </div>
           <div
@@ -38,7 +38,7 @@ function UserInfo(props) {
             }  py-[0.3rem]  px-[1.5rem] self-center `}
           >
             <div>
-              <p className=" text-[#898989]">Pictures</p>
+              <p className=" text-[#A8A8A8]">Pictures</p>
             </div>
           </div>
           <div
@@ -48,7 +48,7 @@ function UserInfo(props) {
             }  py-[0.3rem]  px-[1.8rem] self-center `}
           >
             <div>
-              <p className=" text-[#898989]">Videos</p>
+              <p className=" text-[#A8A8A8]">Videos</p>
             </div>
           </div>
         </div>
