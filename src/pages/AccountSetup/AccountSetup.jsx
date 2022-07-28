3affa11c285e2702282f622836bf6975958ea098
Joan Nobei <nobeijoan1@gmail.com>
2022-07-28 21:03:36 -0100

    account setup

--- src/pages/AccountSetup/AccountSetup.jsx
+++ src/pages/AccountSetup/AccountSetup.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { LoginNav } from "../../components";
 import { bgLogin, woman, male } from "../../assets";
@@ -11,7 +11,7 @@ function AccountSetup() {
   useEffect(() => {
     document.title = "Account SetUp | Kiekky";
   }, []);
-  
+
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data, errors) => {
     console.log(data);
@@ -38,7 +38,7 @@ function AccountSetup() {
           </p>
 
           <div className="gender_select flex flex-col mb-8 ">
-            <input type="radio" name="gender" id="test2" value="I am a man" />
+            <input type="radio" name="gender" id="test2" value="I am a man"  {...register("man")} />
             <label
               className=" flex flex-row items-center justify-between mb-4 outline-none text-[gray] rounded-md pl-5 bg-[#F6F4FF] "
               htmlFor="test2"
@@ -49,7 +49,7 @@ function AccountSetup() {
               </span>
             </label>
 
-            <input type="radio" name="gender" id="test" value="I am a woman" />
+            <input type="radio" name="gender" id="test" value="I am a woman"  {...register("woman")} />
             <label
               className="flex flex-row items-center justify-between outline-none mb-5 text-[gray] rounded-md  pl-5 bg-[#F6F4FF] "
               htmlFor="test"
@@ -70,7 +70,7 @@ function AccountSetup() {
               type="checkbox"
               name="genderM"
               id="test4"
-              value="I am a man"
+              value="I am a man"  {...register("lookingForMale")}
             />
             <label
               className=" flex flex-row items-center justify-between mb-4 outline-none text-[gray] rounded-md pl-5  bg-[#F6F4FF] "
@@ -87,7 +87,7 @@ function AccountSetup() {
               type="checkbox"
               name="genderF"
               id="test3"
-              value="I am a woman"
+              value="I am a woman" {...register("lookingForFemale")}
             />
             <label
               className=" flex items-center flex-row justify-between outline-none mb-5 text-[gray] rounded-md pl-5 bg-[#F6F4FF] "
@@ -97,7 +97,7 @@ function AccountSetup() {
               <span className="  px-[1.3rem] py-[0.6rem] ">
                 <IoMdFemale className="w-full h-[1.7rem]" />
               </span>
-              
+
             </label>
           </div>
 
