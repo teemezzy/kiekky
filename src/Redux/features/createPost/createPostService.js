8b9c83edc4c1a7f5e17af143d0d9da1c03b575d1
Toromo A <toromo.ade06@gmail.com>
2022-08-03 04:58:05 -0500

    useerbalnce

--- src/Redux/features/createPost/createPostService.js
+++ src/Redux/features/createPost/createPostService.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL;
 
-//create new post
 const createPost = async (postData, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  }; 
-  // console.log(config);
+  };
 
   const response = await axios.post(API_URL + "post/create", postData, config);
   return response.data;
--- src/Redux/features/userSetup/setupService.js
+++ src/Redux/features/userSetup/setupService.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-// import token from './setupSlice'
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const getUserSetup = async (setup, token) => {
-  // console.log(token, 'my token')
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
