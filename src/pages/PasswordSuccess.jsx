2464a9fbc76f77acfb78fe7b2f3c4ad5792c54ea
Joan Nobei <nobeijoan1@gmail.com>
2022-08-04 18:24:49 -0100

    account setup succesful page

--- src/pages/PasswordSuccess.jsx
+++ src/pages/PasswordSuccess.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
 
+
+
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import {NavLink} from 'react-router-dom'
+import { LoginNav } from "../components";
+import { bgLogin } from "../assets";
 const PasswordSucess = () => {
-  return (
-    <div>
-        <h1>Password Reset Was Successful </h1>
-        <p>Please login to continue</p>
-        
-    </div>
-  )
-}
+  const { handleSubmit } = useForm();
+  const onSubmit = (data) => {
+  };
 
-export default PasswordSucess
+
+  return (
+    <div className="flex relative w-[352px] lg:w-full m-auto ">
+      <LoginNav className="lg:hidden block" />
+
+      <div className="background  lg:w-2/5 hidden lg:block ">
+        <img className=" h-full" src={bgLogin} alt="Login" />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)} className=" mx-auto">
+        <h1 className=" font-bold text-5xl mt-32 text-[#212121] ">Successful </h1>
+        <p className="my-7 text-[#666666] ">
+          Password reset successful. Please continue to login.
+        </p>
+        
+
+       <NavLink to='/login' > 
+
+       <button className=" text-white w-full mt-10 mb-4 rounded-md py-3 px-auto bg-[#6A52FD] ">
+          Login
+        </button>
+        </NavLink>
+       
+      </form>
+    </div>
+  );
+ };
+
+export default PasswordSucess;
